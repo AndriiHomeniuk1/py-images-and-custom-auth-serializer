1574b2a6ebc084cb13999526c75cf10523d86df6
Andrii Homeniuk <andreygomenuk@gmail.com>
2026-06-30 14:54:49 +0300

    style(admin): fix visual indent in fieldsets!!!

--- user/admin.py
+++ user/admin.py
@@ -11,25 +11,10 @@ class UserAdmin(DjangoUserAdmin):
 
     fieldsets = (
         (None, {"fields": ("email", "password")}),
-        (
-            _("Personal info"),
-            {"fields": ("first_name", "last_name")}
-        ),
-        (
-            _("Permissions"),
-            {"fields": (
-                "is_active",
-                "is_staff",
-                "is_superuser",
-                "groups",
-                "user_permissions"
-                )
-            }
-        ),
-        (
-            _("Important dates"),
-            {"fields": ("last_login", "date_joined")}
-        ),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser",
+                            "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
         (None, {
