4762d0caac3f751735e4bbc27e940cf3c93c9af5
Andrii Homeniuk <andreygomenuk@gmail.com>
2026-06-30 15:13:50 +0300

    style(utils): add type annotation for Movie
    Enabled postponed evaluation of annotations via __future__ import.
    Added proper type hint for instance: Movie in movie_image_path.

--- cinema/utils.py
+++ cinema/utils.py
@@ -1,10 +1,15 @@
+from __future__ import annotations
 import pathlib
 import uuid
 from django.utils.text import slugify
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from cinema.models import Movie
 
 
 def movie_image_path(
-        instance: "Movie", filename: str) -> pathlib.Path:  # noqa: F821
+        instance: Movie, filename: str) -> pathlib.Path:
     filename = (
         f"{slugify(instance.title)}-{uuid.uuid4()}"
         + pathlib.Path(filename).suffix
