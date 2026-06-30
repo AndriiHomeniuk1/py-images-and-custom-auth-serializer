937384de1b0596ab3a48edaff09e94532094e093
Andrii Homeniuk <andreygomenuk@gmail.com>
2026-06-30 11:11:20 +0300

    style(utils): fix flake8 warnings
    - Removed over‑indentation (E126) in movie_image_path
    - Added # noqa: F821 to suppress undefined 'Movie' type hint

--- cinema/utils.py
+++ cinema/utils.py
@@ -3,9 +3,10 @@ import uuid
 from django.utils.text import slugify
 
 
-def movie_image_path(instance: "Movie", filename: str) -> pathlib.Path:
+def movie_image_path(
+        instance: "Movie", filename: str) -> pathlib.Path:  # noqa: F821
     filename = (
-            f"{slugify(instance.title)}-{uuid.uuid4()}"
-            + pathlib.Path(filename).suffix
+        f"{slugify(instance.title)}-{uuid.uuid4()}"
+        + pathlib.Path(filename).suffix
     )
     return pathlib.Path("uploads/movies/") / pathlib.Path(filename)
